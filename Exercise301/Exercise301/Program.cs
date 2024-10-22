8af90f3b1fa506edbd6bb1f598c47512d83ef019
Kaguya Shirayuki <restart.animation@gmail.com>
2024-10-22 17:05:14 +0900

    2024/10/22 午後までに学習した内容

---
+++ Exercise301/Exercise301/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise301
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // dobule型の変数tempを宣言し、キーボードから数値を入力させなさい。
+            Console.Write("整数値を入力: ");
+            double temp = double.Parse(Console.ReadLine());
+            // tempの値を表示しなさい。
+            Console.WriteLine(temp);
+            // tempが0.0ならゼロと表示しなさい。
+            if (temp == 0.0){ Console.WriteLine("ゼロ"); } 
+            // tempが負の数ならマイナスと表示しなさい。
+            else if (temp > 0.0){ Console.WriteLine("プラス"); }  
+            // tempが正の値ならプラスと表示しなさい。
+            else{ Console.WriteLine("マイナス");}
+        }
+    }
+}
---
+++ Exercise302/Exercise302/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise302
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = Console.ReadLine();
+            String s2 = Console.ReadLine();
+            
+            if(s1 == "Hello" && s2 == "World"){
+                Console.WriteLine("Good");
+            }else{
+                Console.WriteLine("Hello World please!");
+            }
+        }
+    }
+}
