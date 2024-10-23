6969a0dc5bb95e87a93e4b57ac412176ac2b3175
Kaguya Shirayuki <restart.animation@gmail.com>
2024-10-23 12:15:28 +0900

    2024/10/23 午前中までに学習したファイル

---
+++ Exercise402/Exercise402/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise402
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力： ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < number; i++)
+            {
+                if(number % i == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exercise403/Exercise403/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise403
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1．2つの1～10の整数の乱数を発生させ、整数型変数a,bに代入
+            Random rnd = new Random();
+            int a = rnd.Next(1, 11);
+            int b = rnd.Next(1, 11);
+           
+            // 2. a= ●, b= × といった具合にa,bの値を表示（例：a=2, b=4）
+            Console.WriteLine("a = {0} b = {1}", a, b);
+
+            // 3. a+b= と表示しその後整数値をコンソールから入力させる
+            while (true)
+            {
+                Console.Write("a + b = ");
+                int ans = int.Parse(Console.ReadLine());
+
+                // 4. 入力した値がa,bの和に等しければ正解と表示しプログラム修了
+                if (ans == a + b)
+                {
+                    Console.WriteLine("正解");
+                    break;
+                }
+                else
+                {   // 5. 正しくなければまちがいと表示し3に戻る
+                    Console.WriteLine("間違い");
+                }
+            }
+        }
+    }
+}
