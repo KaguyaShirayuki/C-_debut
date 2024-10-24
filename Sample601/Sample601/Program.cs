ddb73de0a96515f35f84b21d963dd41514c393b4
Kaguya Shirayuki <restart.animation@gmail.com>
2024-10-24 10:28:24 +0900

    2024/10/23 午後までに学習したファイル

---
+++ Sample601/Sample601/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample601
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1, p2;
+            p1 = new Person();
+            p2 = new Person();
+            p1.name = "伊藤健太郎";
+            p1.age = 50;
+            p2.SetAgeAndName("上田麗奈", 30);
+
+            p1.ShowAgeAndName();
+            p2.ShowAgeAndName();
+
+        }
+    }
+}
